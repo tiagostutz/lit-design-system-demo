47427d3fa38e4a31e419c61ed1b5d95c8fc8e4ac
Tiago Stutz <tiagostutz@gmail.com>
2021-07-09 22:39:15 -0300

    feat: implement container with maxWidth and position attributes. closes #7

--- lit-app/src/design-system/components/main-container/src/MainContainer.ts
+++ lit-app/src/design-system/components/main-container/src/MainContainer.ts
@@ -1,12 +1,56 @@
-import { LitElement, html, customElement } from 'lit-element';
+import { LitElement, css, html, customElement, property } from 'lit-element';
+import { Position } from '../../common-types/positioning';
+import { Breakpoints } from '../../common-types/sizes';
 
 /**
  * @slot content of the title
  */
 @customElement('main-container')
 export class MainContainer extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+      .max-width-none {
+        max-width: 100%;
+      }
+      .max-width-2xl {
+        max-width: 1536px;
+      }
+      .max-width-xl {
+        max-width: 1280px;
+      }
+      .max-width-lg {
+        max-width: 1024px;
+      }
+      .max-width-md {
+        max-width: 768px;
+      }
+      .max-width-sm {
+        max-width: 640px;
+      }
+
+      .mx-auto {
+        margin-left: auto;
+        margin-right: auto;
+      }
+    `;
+  }
+
+  @property({ attribute: 'max-width' })
+  maxWidth: Breakpoints = 'none';
+
+  @property({ attribute: 'position' })
+  position: Position = 'center';
+
   render() {
-    return html`<div><slot></slot></div>`;
+    const maxWidthClass = this.maxWidth ? `max-width-${this.maxWidth}` : '';
+    const positionClass = this.position === 'center' ? `mx-auto` : 'none';
+
+    return html`<div class="${maxWidthClass} ${positionClass}">
+      <slot></slot>
+    </div>`;
   }
 }
 
