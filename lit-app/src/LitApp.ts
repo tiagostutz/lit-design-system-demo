3bdf008f2db06114503b277ddbf63a2956904417
Tiago Stutz <tiagostutz@gmail.com>
2021-07-10 14:12:44 -0300

    fix: add autofocus to EditableInlineMode. closes #3 closes #12

--- lit-app/src/LitApp.ts
+++ lit-app/src/LitApp.ts
@@ -11,7 +11,7 @@ export class LitApp extends LitElement {
       <main>
         <main-container max-width="lg">
           <h1-title>${this.title}</h1-title>
-          <editable-inline-text>Temp</editable-inline-text>
+          <editable-inline-text edit-mode="edit"></editable-inline-text>
         </main-container>
       </main>
     `;
